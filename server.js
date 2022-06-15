90050d6dbdfd9cd3fefa9892bc9ab71e98c904bd
omer <jagenau49@gmail.com>
2022-06-15 12:44:36 +0200

    gets data from api

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 import express from "express";
+import axios from "axios";
 const app = express();
 const port = 3007;
+const nounsFromApi = (
+  await axios.get("https://edwardtanguay.netlify.app/share/germanNouns.json")
+).data;
+const nounsList = `<ol>
+${nounsFromApi
+  .map(
+    (noun) => `<li>${noun.article} ${noun.singular} (pl. ${noun.plural})</li>`
+  )
+  .join("")}
+</ol>`;
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(nounsList);
 });
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
